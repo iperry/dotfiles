092067bb85ab57f5e3ff3b8923519a19fce7519e
Perry Hung <iperry@gmail.com>
2014-09-30 12:16:33 -0400

    cleanup cruft

--- xmonad.hs
+++ xmonad.hs
@@ -9,22 +9,15 @@ import System.Exit
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.WindowNavigation
 import XMonad.Hooks.DynamicLog
-import XMonad.Util.Loggers
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
-import XMonad.Layout.NoBorders
-import XMonad.Actions.TagWindows
-import XMonad.Prompt    -- to use tagPrompt
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.ManageHelpers
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
-
 myLayouts = avoidStruts $ smartBorders $ windowNavigation $ ResizableTall 1 (3/100) (1/2) [] ||| noBorders Full
-
---myDefaultGaps = [(18,0,0,0)] -- 2 gaps on 2 monitors
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
@@ -34,7 +27,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     [ ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
     -- launch dmenu
     , ((modMask,               xK_p     ), spawn "exe=`dmenu_run ` && eval \"exec $exe\"")
-    -- close focused window 
+    -- close focused window
     , ((modMask .|. shiftMask, xK_c     ), kill)
      -- Rotate through the available layout algorithms
     , ((modMask,               xK_space ), sendMessage NextLayout)
@@ -75,29 +68,10 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     , ((modMask              , xK_comma ), sendMessage (IncMasterN 1))
     -- Deincrement the number of windows in the master area
     , ((modMask              , xK_period), sendMessage (IncMasterN (-1)))
-    -- gnome-logout
-    , ((modMask              , xK_period), sendMessage (IncMasterN (-1)))
     -- Quit xmonad
     , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
     -- Restart xmonad
     , ((modMask              , xK_q     ), restart "xmonad" True)
-
-    -- toggle gap
-    , ((modMask .|. controlMask, xK_t     ), sendMessage ToggleStruts)
-
-    -- tagging
-    , ((modMask,                 xK_f  ), withFocused (addTag "abc"))
-    , ((modMask .|. controlMask, xK_f  ), withFocused (delTag "abc"))
-    , ((modMask .|. shiftMask,   xK_f  ), withTaggedGlobalP "abc" W.sink)
-    , ((modMask,                 xK_d  ), withTaggedP "abc" (W.shiftWin "2"))
-    , ((modMask .|. shiftMask,   xK_d  ), withTaggedGlobalP "abc" shiftHere)
-    , ((modMask .|. controlMask, xK_d  ), focusUpTaggedGlobal "abc")
-    , ((modMask,                 xK_g  ), tagPrompt defaultXPConfig (\s -> withFocused (addTag s)))
-    , ((modMask .|. controlMask, xK_g  ), tagDelPrompt defaultXPConfig)
-    , ((modMask .|. shiftMask,   xK_g  ), tagPrompt defaultXPConfig (\s -> withTaggedGlobal s float))
---    , ((modMask,                 xK_g  ), tagPrompt defaultXPConfig (\s -> withTaggedP s (W.shiftWin "2")))
---    , ((modMask .|. shiftMask,   xK_g  ), tagPrompt defaultXPConfig (\s -> withTaggedGlobalP s shiftHere))
---    , ((modMask .|. controlMask, xK_g ), tagPrompt defaultXPConfig (\s -> focusUpTaggedGlobal s))
     ]
     ++
 
