6a634b98611974ad6aad14221e1357800c36d47c
Perry Hung <iperry@gmail.com>
2015-03-16 16:24:05 -0400

    zsh: add MyAntigen file for antigen-hs

---
+++ MyAntigen.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+module MyAntigen where
+
+import Antigen (
+                -- Rudimentary imports
+                AntigenConfiguration (..)
+              , bundle
+              , antigen
+                -- If you want to source a bit trickier plugins
+              , ZshPlugin (..)
+              , antigenSourcingStrategy
+              , filePathsSourcingStrategy
+              )
+import Shelly (shelly)
+
+bundles =
+  [ bundle "Tarrasch/zsh-functional"
+  , bundle "Tarrasch/zsh-bd"
+  , bundle "zsh-users/zsh-syntax-highlighting"
+  , bundle "zsh-users/zsh-history-substring-search"
+
+  -- If you use a plugin that doesn't have a *.plugin.zsh file. You can set a
+  -- more liberal sourcing strategy.
+  --
+  -- , (bundle "some/stupid-plugin") { sourcingStrategy = antigenSourcingStrategy }
+
+  -- If you use a plugin that has sub-plugins. You can specify that as well
+  --
+  -- NOTE: If you want to use oh-my-zsh for real (please don't), you still need
+  -- to set the $ZSH env var manually.
+  , (bundle "robbyrussell/oh-my-zsh")
+     { sourcingStrategy = filePathsSourcingStrategy
+                           [ "lib/git.zsh" ] }
+
+  -- Sourcing a list of files
+  -- , (bundle "alfredodeza/zsh-plugins")
+  --    { sourcingStrategy = filePathsSourcingStrategy
+  --                          [ "vi/zle_vi_visual.zsh"
+  --                          , "pytest/pytest.plugin.zsh"
+  --                          ] }
+
+  -- Alternatively, this way will give you the same result
+  -- , (bundle "alfredodeza/zsh-plugins")
+  --    { sourcingStrategy = antigenSourcingStrategy
+  --    , sourcingLocations = [ "vi"
+  --                          , "pytest"
+  --                          ] }
+
+  -- vvv    Add your plugins here    vvv
+  ]
+
+config = AntigenConfiguration bundles
+
+main :: IO ()
+main = shelly $ antigen config
