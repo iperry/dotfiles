6be34ab44f44bee11c788c3c02e2d864de86c730
Perry Hung <perry@mosi.io>
2022-07-07 11:44:34 -0700

    scripts: clean up ubuntu cruft

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -6,18 +6,14 @@ apt-get install wget curl neovim python-dev python-pip python3-dev python3-pip x
 
 pip install neovim
 
-# fonts:
-mkdir -p ~/.fonts
-wget https://github.com/chrissimpkins/Hack/releases/download/v2.019/Hack-v2_019-ttf.zip -O /tmp/hack.zip
-unzip /tmp/hack.zip -d ~/.fonts
-fc-cache -fv
-
 apt-get install zsh
 
 # development stuff
-apt-get install git-core clang ccache
-
-apt-get install cmake
-
-add-apt-repository ppa:h-realh/roxterm
-apt-get install roxterm
+apt-get install \
+  git-core      \
+  clang         \
+  clangd        \
+  clang-format  \
+  ccache        \
+  cmake         \
+  ripgrep
