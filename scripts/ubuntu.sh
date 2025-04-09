2e22a3290fb587fc90d3211b2197f776b6283b74
Perry Hung <perry@mosi.io>
2025-04-08 21:09:31 -0700

    scripts: update ubuntu setup script

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-add-apt-repository ppa:neovim-ppa/unstable
+add-apt-repository ppa:neovim-ppa/stable
 apt-get update
-apt-get install wget curl neovim python-dev python-pip python3-dev python3-pip xclip
+apt-get install wget curl neovim python3-pip python3-dev python3-pip
 
-pip install neovim
+pip3 install neovim
 
 apt-get install zsh
 
