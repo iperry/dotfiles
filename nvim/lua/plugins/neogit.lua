62efab254ccba943aead9d566f1734c8846d6249
Perry Hung <perry@mosi.io>
2025-11-24 18:59:24 -0800

    nvim: add shortcut for neogit

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -1,10 +1,14 @@
 return {
   "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
+    "nvim-lua/plenary.nvim",  -- required
+    "sindrets/diffview.nvim", -- optional - Diff integration
 
     -- Only one of these is needed.
     "nvim-telescope/telescope.nvim", -- optional
+  },
+
+  keys = {
+    { "<leader>ng", "<cmd>Neogit<cr>", desc = "Neogit" },
   },
 }
